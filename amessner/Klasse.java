ed1221eaab85d06afb062620af8d6f6ff18c3c7e
amessner <alexander.messner.am@gmail.com>
2017-03-20 13:59:44 +0100

    System.out.print("Änderung!");

--- amessner/Klasse.java
+++ amessner/Klasse.java
@@ -5,7 +5,8 @@ public class Klasse {
 	public static void main(String[] args) {
 		System.out.println("Hallo!");
 		System.out.println("Torkamani");
-
+		System.out.println("Änderung!");
+		System.out.println("Änderung!");
 	}
 
 }
